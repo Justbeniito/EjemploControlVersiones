47ebe1d98f450f7f427a849fd8d017c1d0cb5e18
Justbeniito <justbefa@alumni.uv.es>
2026-02-07 17:14:20 +0100

    Factor común 2

---
+++ Perim.R
@@ -0,0 +1,11 @@
+PerimetroCuadrado <- function(L){
+  return(4*L)
+}
+
+LongitudCircunferencia <- function(R){
+  return(2*3.14159*R)
+}
+
+PerimetroRectangulo <- function(L, l){
+  return(2*(L+l))
+}
